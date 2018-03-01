b36a2be5fa84c294b811cd33854c757abcb8b1e6
taoism <taoism@taoism.chen>
2018-03-01 15:00:15 +0800

    package

---
+++ src/main/java/com/qawhcb/shadow/enumeration/test.java
@@ -0,0 +1,8 @@
+package com.qawhcb.shadow.enumeration;
+
+/**
+ * @author Taoism <br/>
+ * Created on 2018/3/1 <br/>
+ */
+public class test {
+}
---
+++ src/main/java/com/qawhcb/shadow/utils/test.java
@@ -0,0 +1,8 @@
+package com.qawhcb.shadow.utils;
+
+/**
+ * @author Taoism <br/>
+ * Created on 2018/3/1 <br/>
+ */
+public class test {
+}
