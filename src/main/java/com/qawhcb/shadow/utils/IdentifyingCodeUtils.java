5e8ccc8057b8e0a54291e6cc8b957f78d4b5071b
taoism <taoism@taoism.chen>
2018-03-08 10:30:25 +0800

    提交

--- src/main/java/com/qawhcb/shadow/utils/IdentifyingCodeUtils.java
+++ src/main/java/com/qawhcb/shadow/utils/IdentifyingCodeUtils.java
@@ -17,8 +17,9 @@ public class IdentifyingCodeUtils {
 		}
 
 		try {
-			SMSUtils.send(phone, " 尊敬的用户，您的短信验证码是" + code + "。请尽快完成验证。若非本人发送，请忽略此短信。");
-		} catch (Exception e) {
+            String send = SMSUtils.send(phone, "【爱影动萌】尊敬的用户，您的短信验证码是" + code + "。请尽快完成验证。若非本人发送，请忽略此短信。回复TD退订！");
+            System.out.println(send);
+        } catch (Exception e) {
 			LoggerUtil.getLogger().error("验证码生成错误！");
 		}
 
---
+++ src/test/java/com/qawhcb/shadow/TestController.java
@@ -0,0 +1,8 @@
+package com.qawhcb.shadow;
+
+/**
+ * @author Taoism <br/>
+ * Created on 2018/3/8 <br/>
+ */
+public class TestController {
+}
